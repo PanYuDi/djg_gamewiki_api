eb4d6e0adcb564d03d441f145c5345acd9dbbc6a
PanYuDi <826941817@qq.com>
2024-04-11 07:45:54 +0800

    remove swagger

--- src/main/java/com/tencent/wxcloudrun/controller/ItemListController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/ItemListController.java
@@ -4,8 +4,6 @@ import com.tencent.wxcloudrun.config.ApiResponse;
 import com.tencent.wxcloudrun.model.ItemModel;
 import com.tencent.wxcloudrun.service.ItemListService;
 import com.tencent.wxcloudrun.vo.ItemListResponse;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
